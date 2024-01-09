508215f893ad271d8f8d67d725fcbbd7d4bb2390
Will Dixon <wdixon@bizstream.com>
2024-01-08 21:47:56 -0500

    stats page makes steamRouter call

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,17 +1,20 @@
 import { Flex, Heading } from "@chakra-ui/react";
+import steamRouter, { NextSteamAuthApiRequest } from "@lib/steamRouter";
+import { NextApiResponse } from "next";
 
 // make a getServerSideProps function that fetches the data from the API
-export async function getServerSideProps() {
-  // call the Steam API to authorize/sign in
-  //   const res = await fetch(
-  //     "https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=6C7E8C5C7F6B9B6F6B9B1D1D5F5E7E5E&steamid=76561198045577318"
-  //   );
-  //   const data = await res.json();
-  //   return {
-  //     props: {
-  //       data,
-  //     },
-  //   };
+export async function getServerSideProps({
+  req,
+  res,
+}: {
+  req: NextSteamAuthApiRequest;
+  res: NextApiResponse;
+}) {
+  await steamRouter.run(req, res);
+
+  console.log(req.user); // Shows the SteamProfile object in console.
+
+  return { props: { user: req.user || null } };
 }
 
 export function Stats({ Component, pageProps }) {
