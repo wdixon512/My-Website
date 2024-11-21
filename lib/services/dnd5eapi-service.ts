520f2ff12214b10f0a808adfd53f66f5ba26e50d
wdixon <wdixon@bizstream.com>
2024-11-21 01:29:46 -0500

    client side cache context working, still can make improvements

--- lib/services/dnd5eapi-service.ts
+++ lib/services/dnd5eapi-service.ts
@@ -1,3 +1,4 @@
+import { useClientCache } from '@lib/components/contexts/CacheContext';
 import { RollType, RollTypeMethods } from '@lib/models/dm-helper/RollType';
 import { AllMonstersResponse } from '@lib/models/dnd5eapi/AllMonstersResponse';
 import { DetailedMob, SummaryMob } from '@lib/models/dnd5eapi/DetailedMob';
@@ -16,6 +17,7 @@ interface UseDndApiHook {
 // Custom hook to interact with the D&D 5e API
 export const useDndApi = (): UseDndApiHook => {
   const [summaryMobs, setSummaryMobs] = useState<SummaryMob[]>([]);
+  const { cacheLoadAsync } = useClientCache();
 
   useEffect(() => {
     // Load a list of monsters when the hook is first used
@@ -25,28 +27,33 @@ export const useDndApi = (): UseDndApiHook => {
         if (!response.ok) {
           throw new Error('Failed to fetch monsters');
         }
-        const monsterData: AllMonstersResponse = await response.json();
-        setSummaryMobs(monsterData.monsters);
+        return (await response.json()) as AllMonstersResponse;
       } catch (error) {
         console.error('Failed to fetch monsters:', error);
       }
     };
 
-    fetchMonsters();
+    cacheLoadAsync('allMonsters', fetchMonsters).then((monsterData) => {
+      setSummaryMobs(monsterData.monsters);
+    });
   }, []);
 
   // Fetch details of a specific monster by its index
   const getMobByName = async (mobName: string): Promise<DetailedMob | null> => {
-    try {
-      const response = await fetch(`${BASE_URL}/${mobName.replace(' ', '_').toLowerCase()}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch monster details');
+    const cacheKey = mobName.replace(' ', '_').toLowerCase();
+
+    return cacheLoadAsync(cacheKey, async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/${mobName.replace(' ', '_').toLowerCase()}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch monster details');
+        }
+        return (await response.json()) as DetailedMob;
+      } catch (error) {
+        console.error('Failed to fetch monster details:', error);
+        return null;
       }
-      return (await response.json()) as DetailedMob;
-    } catch (error) {
-      console.error('Failed to fetch monster details:', error);
-      return null;
-    }
+    });
   };
 
   const rollDice = (mob: DetailedMob, rollType: RollType): number => {
